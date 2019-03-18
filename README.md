8d553efd969dfa532eeb124f728fe8074809a1fe
Martin Rotter <rotter.martinos@gmail.com>
2019-03-18 08:35:23 +0100

    push to influx

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 Mini Python-based server which collects temp/humidity data fromy my sensors and stores them into file.
 
 See `udpserver.py` for info.
-
-## weather-server
-Simple one-page website which reads temp/humidity data and serves it for easy display via [Dygraphs](http://dygraphs.com).
-
-Run with `python3 -m http.server <port>`
--- pc-info-server/pcserver.py
+++ pc-info-server/pcserver.py
@@ -1,7 +1,3 @@
-## python pcserver.py <data_output.csv>
-## run as:
-##    python pcserver.py "../weather-server/data/pc-data-TIME.csv"
-
 import logging
 import sys
 import os
@@ -15,26 +11,15 @@ log = logging.getLogger("temp_server")
 format_cons = "%(asctime)s %(name)-12s %(levelname)8s\t%(message)s"
 logging.basicConfig(level = logging.DEBUG, format = format_cons)
 
-# Note that placeholder TIME is replaced with current
-# "year-month" (for example "2018-07") for each request.
-time_placeholder = "TIME"
-data_file_mask = os.getcwd() + '/' + sys.argv[1]
-csv_header = "DateTime,CpuTemp,MemoryPerc"
-
 while True:
-  # Create path to correct CSV file.
-  dat_now = datetime.datetime.now()
-  real_file = data_file_mask.replace(time_placeholder, dat_now.strftime('%Y-%m'))
+  sens_temp = psutil.sensors_temperatures()["coretemp"][0].current
+  sens_mem = psutil.virtual_memory().percent
+ 
+  # Submit the data.
+  formatted_data = "influx -database 'home_data' -execute 'INSERT server temp={},mem_perc={}'".format(sens_temp, sens_mem)
+
+  print(formatted_data)
+
+  os.system(formatted_data)
   
-  # If file does not exist, append the header.
-  if not os.path.isfile(real_file):
-    print("File %r does not exist, appending CSV header." % (real_file))
-    with open(real_file, "a") as myfile:
-      myfile.write(csv_header + "\r\n")
-  
-  # Append the data.
-  formatted_data = "%s,%s,%s" % (dat_now.strftime('%Y-%m-%dT%H:%M'), str(psutil.sensors_temperatures()["coretemp"][0].current), str(psutil.virtual_memory().percent))
-  with open(real_file, "a") as myfile:
-    myfile.write(formatted_data + "\r\n")
-  
-  time.sleep(60)
+  time.sleep(30)

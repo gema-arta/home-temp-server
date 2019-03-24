06d68d1d631ce4616b788f2145371ebecd90c563
Martin Rotter <rotter.martinos@gmail.com>
2019-03-24 21:12:52 +0100

    zakoment

--- consume-data-server/udpserver.py
+++ consume-data-server/udpserver.py
@@ -1,8 +1,5 @@
-## python udpserver.py <data_output.csv> <udp_port>
-## run as:
-##    python udpserver.py "../weather-server/data/sample-data.csv" 12345
-##    or with year placeholder which will create file-per-year
-##    python udpserver.py "../weather-server/data/sample-data-%year%.csv" 12345
+## python udpserver.py <udp_port>
+## run as: python udpserver.py  12345
 ##
 ## send sample UDP packet.
 ##
@@ -20,19 +17,13 @@ format_cons = "%(asctime)s %(name)-12s %(levelname)8s\t%(message)s"
 logging.basicConfig(level = logging.DEBUG, format = format_cons)
 
 # Global variables.
-server_port = int(sys.argv[2])
-
-# Note that placeholder %year% is replaced with current
-# year for each request if contained in argument.
-year_placeholder = "%year%"
-data_file_mask = os.getcwd() + '/' + sys.argv[1]
+server_port = int(sys.argv[1])
 
 def udp_server(host = "0.0.0.0", port = server_port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     log.info("Listening on 'udp/%s:%s'..." % (host, port))
-    log.info("Will append data to %r file mask.\n" % (data_file_mask))
     
     s.bind((host, port))
     while True:

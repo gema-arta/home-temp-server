a1640298e51201d101310fee8cab2e9b082d3a35
Martin Rotter <rotter.martinos@gmail.com>
2020-06-29 19:34:00 +0200

    Cleanups.

--- net-info-server/netinfo.py
+++ net-info-server/netinfo.py
@@ -1,25 +1,26 @@
 import subprocess
 import os
 
-result = subprocess.run(['speedtest-cli', '--csv'], stdout=subprocess.PIPE, text=True)
+while True:
+  result = subprocess.run(['speedtest-cli', '--csv'], stdout=subprocess.PIPE, text=True)
+  
+  if result.returncode == 0:
+    print("Obtained speedtest data is: '%s'." % (result.stdout))
 
-if result.returncode == 0:
-  print("Obtained speedtest data is: '%s'." % (result.stdout))
+    # Parse the data.
+    parsed_result = result.stdout.replace("\n", "")
+    parsed_list = parsed_result.split(",")
 
-  # Parse the data.
-  parsed_result = result.stdout.replace("\n", "")
-  parsed_list = parsed_result.split(",")
+    ip_addr = parsed_list[9]
+    download = parsed_list[6]
+    upload = parsed_list[7]
+    ping = parsed_list[5]
 
-  ip_addr = parsed_list[9]
-  download = parsed_list[6]
-  upload = parsed_list[7]
-  ping = parsed_list[5]
+    print("IP address: %s\nDownload: %s\nUpload: %s\nPing: %s" % (ip_addr, download, upload, ping))
 
-  print("IP address: %s\nDownload: %s\nUpload: %s\nPing: %s" % (ip_addr, download, upload, ping))
+    # Send data to database.
+    db_command = "influx -database 'home_data' -execute 'INSERT netspeed,ipaddr=\"{}\" download={},upload={},ping={}'".format(ip_addr, download, upload, ping) 
 
-  # Send data to database.
-  db_command = "influx -database 'home_data' -execute 'INSERT netspeed,ipaddr=\"{}\" download={},upload={},ping={}'".format(ip_addr, download, upload, ping) 
-
-  os.system(db_command)
-else:
-  print("Error running 'speedtest-cli'.")
+    os.system(db_command)
+  else:
+    print("Error running 'speedtest-cli'.")
--- pc-info-server/pcserver.py
+++ pc-info-server/pcserver.py
@@ -14,12 +14,11 @@ logging.basicConfig(level = logging.DEBUG, format = format_cons)
 while True:
   sens_temp = psutil.sensors_temperatures()["coretemp"][0].current
   sens_mem = psutil.virtual_memory().percent
- 
+  
   # Submit the data.
   formatted_data = "influx -database 'home_data' -execute 'INSERT server temp={},mem_perc={}'".format(sens_temp, sens_mem)
-
-  print(formatted_data)
-
-  os.system(formatted_data)
   
-  time.sleep(30)
+  print(formatted_data)
+  
+  os.system(formatted_data)
+  time.sleep(60*60*3)
